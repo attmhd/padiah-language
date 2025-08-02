f7a97a134cbe4a54abf7643c97e0d7fc3d9e2bd0
attmhd <2022610013.attan@itp.ac.id>
2025-08-02 19:08:55 +0700

    Fix console script name in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'padang = padiah.executor:main',
+            'padiah = padiah.executor:main',
         ],
     },
     author='Muhammad Attan',
